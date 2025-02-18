355e6ab78ee5c7d95caffbd6e805f492a603c596
XW14TW <xw14tw@student.aau.dk@ailab-fe02.srv.aau.dk>
2025-02-18 13:59:21 +0100

    removed duplicates in csv file using dask

--- main.py
+++ main.py
@@ -3,25 +3,17 @@ import os
 import time
 from duplicate import remove_duplicate
 
-def count_rows_columns(file_path):
-    df = pd.read_csv(file_path)
-    rows, columns = df.shape
 
-    # Remove duplicate rows
-    remove_duplicate(df)
-    
-    return rows, columns
+def main ():
+    start_time = time.time()
 
-print("Reading the CSV file...")
-start_time = time.time()
+    print("Reading the CSV file...")
+    cleaned = remove_duplicate('aisdk-2025-02-14.csv')
 
-# Get the directory of the current script
-script_dir = os.path.dirname(__file__)
-# Construct the full path to the CSV file
-file_path = os.path.join(script_dir, 'aisdk-2025-02-14.csv')
-print(f"File path: {file_path}")
+    print(f"Execution time: {time.time() - start_time} seconds")
 
-rows, columns = count_rows_columns(file_path)
-print(f"Number of rows: {rows}")
-print(f"Number of columns: {columns}")
-print(f"Execution time: {time.time() - start_time} seconds")
+
+if __name__ == '__main__':
+    main()
+
+
