2c8788841ab8759831799b81ef5904f3129c1675
XW14TW <xw14tw@student.aau.dk@ailab-fe02.srv.aau.dk>
2025-02-18 15:19:06 +0100

    We have now cleaned the file by removing redudant colums and duplicates

--- cleaning.py
+++ cleaning.py
@@ -1,9 +1,10 @@
 import dask.dataframe as dd
+import sys
 
 # Configuration
-OUTPUT_CSV = 'unique_data.csv'
+OUTPUT_CSV = 'cleaned.csv'
 
-def remove_duplicate(fileName):
+def cleaning(fileName):
     # Define explicit dtypes for all columns in the CSV
     DTYPES = {
         '# Timestamp': 'object',  # Use 'object' for string or mixed types
@@ -13,8 +14,16 @@ def remove_duplicate(fileName):
         'Name': 'object',         # Use 'object' for string or mixed types
     }
 
+    # List the columns you want to drop
+    columns_to_remove = ['Heading', 'IMO', 'Callsign', 'Name', 'Cargo type',
+                        'Width', 'Length', 'Type of position fixing device', 'Data source type', 'A',
+                        'B', 'C', 'D']
+
     # Read the CSV file using Dask with explicit dtypes
     df = dd.read_csv(fileName, dtype=DTYPES)
+
+    # Drop the redundant columns
+    df = df.drop(columns=columns_to_remove)
     
     # Drop duplicates based on the two columns
     df_unique = df.drop_duplicates(subset=['# Timestamp', 'MMSI'])
@@ -25,4 +34,10 @@ def remove_duplicate(fileName):
     print(f"Duplicate rows removed and saved to {OUTPUT_CSV}")
     return df_unique
 
-    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python cleaning.py <filename>")
+        sys.exit(1)
+
+    fileName = sys.argv[1]
+    cleaning(fileName)
