ad33be1691403a4e7870e3f4972748786cf140f6
XW14TW <xw14tw@student.aau.dk@ailab-fe02.srv.aau.dk>
2025-02-19 13:25:36 +0100

    clean up

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import pandas as pd
-import os
 import time
-from cleaning import cleaning
-import dask.dataframe as dd
 
-def main ():
+def main():
     start_time = time.time()
 
     print(f"Execution time: {time.time() - start_time} seconds")
 
-
 if __name__ == '__main__':
     main()
-
-
